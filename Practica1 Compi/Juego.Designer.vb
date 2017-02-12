3c175a03f41d379ba30a071f29a699ffa0cdb1cc
Dannek <Dannek@DANNEK-PC>
2017-02-12 13:56:57 -0600

    Jugador (disparos, musica y movimientos)

--- Practica1 Compi/Juego.Designer.vb
+++ Practica1 Compi/Juego.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Juego
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Juego
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
@@ -30,7 +30,8 @@ Partial Class Juego
         '
         'PictureBox1
         '
-        Me.PictureBox1.Location = New System.Drawing.Point(59, 27)
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.Location = New System.Drawing.Point(44, 12)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(600, 400)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
@@ -39,15 +40,16 @@ Partial Class Juego
         '
         'Timer1
         '
-        Me.Timer1.Interval = 1000
         '
         'Juego
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(720, 435)
+        Me.ControlBox = False
         Me.Controls.Add(Me.PictureBox1)
         Me.Name = "Juego"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Juego"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
--- Practica1 Compi/Juego.vb
+++ Practica1 Compi/Juego.vb
@@ -3,24 +3,35 @@
 Public Class Juego
 
     Dim tablero As Graphics
-    Dim Imagen As Image = Image.FromFile(LFondos.Aux.fondo)
+    Dim Fondo As Image = Image.FromFile(LFondos.Aux.fondo)
+    Dim Nave As Image = Image.FromFile(LNaves.Aux.Rnave)
+    Dim DisparoN As Image = Image.FromFile(LNaves.Aux.Rdisparo)
+
     Dim Lapiz As Pen
     Dim Logica(10, 10) As String
-    Dim x, y As Integer
+    Dim x, y, posx As Integer
+    Dim pausa As Boolean = False
+
 
     Private Sub Juego_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         tablero = PictureBox1.CreateGraphics
-        PictureBox1.Image = Imagen
+        PictureBox1.Image = Fondo
+        My.Computer.Audio.Play(LFondos.Aux.musica, AudioPlayMode.BackgroundLoop)
+
         x = 0
         y = 0
+        posx = 0
 
         For x = 0 To 9
             For y = 0 To 9
 
-                Logica(x, y) = "s"
+                Logica(x, y) = "0"
 
             Next
         Next
+
+        Logica(posx, 9) = "x"
+
         x = 0
         y = 0
         Timer1.Start()
@@ -29,14 +40,52 @@ Public Class Juego
 
     End Sub
 
+    Private Sub Juego_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If (e.KeyData = Keys.Right) Then
+            If (posx < 9) Then
+                Logica(posx, 9) = "0"
+                posx = posx + 1
+                Logica(posx, 9) = "x"
+            End If
+        ElseIf (e.KeyData = Keys.Left) Then
+            If (posx > 0) Then
+                Logica(posx, 9) = "0"
+                posx = posx - 1
+                Logica(posx, 9) = "x"
+            End If
+        ElseIf (e.KeyData = Keys.Space) Then
+            Logica(posx, 8) = "d"
+            tablero.DrawImage(DisparoN, (posx * 60), (8 * 40), 60, 40)
+            My.Computer.Audio.Play(LNaves.Aux.RSonido, AudioPlayMode.Background)
 
+        End If
+    End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+
+        PictureBox1.Refresh()
+
         Lapiz = New Pen(Brushes.Black, 10)
 
         'cuadricula 10x10, cuadros de 60 *40
+        For x = 0 To 9
+            For y = 0 To 9
+
+                If (Logica(x, y).Equals("x")) Then
+                    tablero.DrawImage(Nave, (x * 60), (y * 40), 60, 40)
+                ElseIf (Logica(x, y).Equals("d")) Then
+                    Logica(x, y) = "0"
+                    tablero.DrawImage(DisparoN, (x * 60), (y * 40), 60, 40)
+                    If (y <> 0) Then
+                        Logica(x, y - 1) = "d"
+                    End If
+
+                End If
 
 
+            Next
+        Next
 
     End Sub
 End Class
